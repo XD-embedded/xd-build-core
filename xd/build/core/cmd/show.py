c32cc3554e004bcfe5236ceded2a5dcc2db3ce4c
Esben Haabendal <esben@haabendal.dk>
2014-10-18 17:31:25 +0200

    New command: show
    Initial implementation for command to select, parse, and show recipe data.
    This initial implementation does not actually parsen and show the recipe
    data.
    
    Signed-off-by: Esben Haabendal <esben@haabendal.dk>

---
+++ xd/build/core/cmd/show.py
@@ -0,0 +1,23 @@
+from xd.build.core.manifest import *
+import os
+
+import logging
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+
+parser_help = 'Show recipe specification'
+
+def add_arguments(parser):
+    parser.add_argument(
+        'recipe',
+        help='recipe (name or path) to show')
+    return
+
+def run(args, manifest, env):
+    manifest = Manifest(manifest)
+    recipe = manifest.get_recipe(args.recipe)
+    if recipe is None:
+        log.error('recipe not found: %s', args.recipe)
+        return 1
+    print('#%s# %s%s#'%(os.linesep, recipe.path, os.linesep))
+    return
